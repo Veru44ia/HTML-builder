7c6ab17eb7faad741c0c6d7bde5492a19e437f06
Veru44ia <vera3188@gmail.com>
2022-11-02 18:43:12 +0300

    feat: add solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.stat(path.join(__dirname, 'files-copy'), err => {
+  if (!err) {
+    fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, 
+      (err) => {
+        if (err) throw err; 
+        copyDir();
+      }
+    );    
+  } else {
+    copyDir();
+  }
+});
+
+function copyDir() {
+  fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, 
+    (err) => {
+      if (err) throw err;
+    }
+  );
+  fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true},
+    (err, data) => {
+      if (err) throw err;
+      data.forEach((element) => {
+        if (element.isFile() === true) {
+          fs.copyFile(path.join(__dirname, 'files', `${element.name}`), path.join(__dirname, 'files-copy', `${element.name}`),
+            (err) => {
+              if (err) throw err;
+            }
+          );
+        }
+      });
+    }
+  );
+}
